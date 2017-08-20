3818555a6cd57d4fbf9a8981c5e2e4617af23bda
sfg1991 <sfg1991@163.com>
2017-08-20 16:58:13 +0800

    Complete to show initial columns

--- code/6_bubble_sort_with_animation_in_js/script.js
+++ code/6_bubble_sort_with_animation_in_js/script.js
@@ -9,11 +9,14 @@
   var padWidth = screenWidth * .7;
   var padHeigth = screenHeight;
   var padding = 20;
-  var columnWidth = 20;
+  var colWidth = 40;
   var minSpace = 10;
-  var maxArrSize = parseInt((padWidth - padding * 2) / (minSpace + columnWidth));
+  var maxArrSize = parseInt((padWidth - padding * 2) / (minSpace + colWidth));
+  var maxColHeight = screenHeight - padding * 2;
+  var minColHeight = 20;
   var arrSize;
   var arr;
+  var colPoses = [];
 
   pad.style.height = screenHeight + 'px';
   pad.style.width = screenWidth * .7 + 'px';
@@ -31,11 +34,40 @@
       if (arrSize >= 3 && arrSize <= maxArrSize) {
         arr = [];
         var count = arrSize;
+        var colGap = (padWidth - colWidth * arrSize) / (arrSize + 1);
+        var colPos = colGap;
+        // Generate the random numbers array and also the column positions array,
+        // and show those numbers in the initial positions
         while (count--) {
-          arr.push(parseInt(Math.random() * maxArrSize) + 3);
+          var num = parseInt(Math.random() * maxColHeight);
+          if (num < minColHeight) {
+            num += minColHeight;
+          }
+          if (num > maxColHeight) {
+            num = maxColHeight;
+          }
+          arr.push(num);
+          colPoses.push(colPos);
+          createCol(num, colPos);
+          colPos += colWidth + colGap;
         }
         console.log('Generated %O', arr);
       }
     }
   });
+
+  /**
+   * Create a col of value num in positon pos.
+   */
+  var createCol = function(num, pos) {
+    var col = document.createElement('div');
+    col.className = 'anim-col';
+    col.style.width = colWidth + 'px';
+    col.style.height = num + 'px';
+    col.style.left = pos + 'px';
+    var colNum = document.createElement('p');
+    colNum.innerText = num;
+    col.appendChild(colNum);
+    pad.appendChild(col);
+  };
 })();
