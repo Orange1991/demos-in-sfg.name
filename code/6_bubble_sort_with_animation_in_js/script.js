5e03ce20e4911559cbac22ba415daf9fc6ea79ef
sfg1991 <sfg1991@163.com>
2017-08-20 21:48:09 +0800

    Add logic of animation, not completed

--- code/6_bubble_sort_with_animation_in_js/script.js
+++ code/6_bubble_sort_with_animation_in_js/script.js
@@ -5,6 +5,11 @@
   var maxSizeContainer = document.getElementById('max-size-container');
   var arrSizeInput = document.getElementById('array-size');
   var btnGenArr = document.getElementById('generate-array');
+  var btnPlay = document.getElementById('btn-play');
+  var btnPause = document.getElementById('ben-pause');
+  var btnRound = document.getElementById('btn-next-round');
+  var btnStep = document.getElementById('btn-next-step');
+  var rdoAsc = document.getElementById('rdo-asc');
 
   var padWidth = screenWidth * .7;
   var padHeigth = screenHeight;
@@ -14,9 +19,28 @@
   var maxArrSize = parseInt((padWidth - padding * 2) / (minSpace + colWidth));
   var maxColHeight = screenHeight - padding * 2;
   var minColHeight = 20;
+  var isAsc;
   var arrSize;
   var arr;
-  var colPoses = [];
+  var colPoses;
+  var colElems;
+
+  // Variables to control the animation progress
+  var MODE = {
+    ALL: 0,
+    ROUND: 1,
+    STEP: 2
+  };
+  var mode = MODE.ALL;
+  var shouldPause;
+  var isRunning;
+  var isFinished;
+  var totalStep;
+  var totalStepThd;
+  var round;
+  var roundThd;
+  var step;
+  var stepThd;
 
   pad.style.height = screenHeight + 'px';
   pad.style.width = screenWidth * .7 + 'px';
@@ -29,10 +53,25 @@
     } catch (e) {
       alert('You input an invalid number');
     }
+
+    totalStepThd = arrSize * (arrSize - 1) / 2;
+    totalStep = 0;
+    step = 0;
+    round = 0;
+    roundThd = arrSize - 1;
+    stepThd = rounds;
+    shouldPause = false;
+    isRunning = false;
+    isFinished = false;
+
     if (arrSize || arrSize === 0) {
       arrSizeInput.value = arrSize;
       if (arrSize >= 3 && arrSize <= maxArrSize) {
         arr = [];
+        colPoses = [];
+        colElems = [];
+        isAsc = !rdoAsc || rdoAsc.checked;
+        console.log(isAsc);
         var count = arrSize;
         var colGap = (padWidth - colWidth * arrSize) / (arrSize + 1);
         var colPos = colGap;
@@ -56,7 +95,46 @@
     }
   });
 
-  /**
+  btnPlay.addEventListener('click', function() {
+    if (!isFinished && !shouldPause && !isRuning) {
+      isRunning = true;
+      btnPause.disabled = false;
+      btnPlay.disabled = true;
+      btnRound.disabled = true;
+      btnStep.disabled = true;
+      runNextStep();
+    }
+  });
+
+  btnPause.addEventListener('click', function() {
+    if (!isFinished && isRunning) {
+      shouldPause = true;
+      btnPause.disabled = true;
+      btnPlay.disabled = true;
+      btnRound.disabled = true;
+      btnStep.disabled = true;
+    }
+  });
+
+  btnRound.addEventListener('click', function() {
+    
+  });
+
+  btnStep.addEventListener('click', function() {
+    
+  });
+
+  var runNextStep = function() {
+    if (isFinished || totalStep >= total) {
+      isFinished = true;
+    } else if (step >= stepThd) {
+      runNextRound();
+    } else if (shoudPause) {
+      
+    } else {
+    }
+  };
+/**
    * Create a col of value num in positon pos.
    */
   var createCol = function(num, pos) {
@@ -69,5 +147,6 @@
     colNum.innerText = num;
     col.appendChild(colNum);
     pad.appendChild(col);
+    colElems.push(col);
   };
 })();
